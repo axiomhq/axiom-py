9682411c9ba37ab5429f7ee4f0969b03e9703482
Islam Shehata <49557793+schehata@users.noreply.github.com>
2021-11-17 23:33:25 +0100

    convert the tests to unittest

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,13 +1,17 @@
 """This module contains the tests for the DatasetsClient."""
 import os
+import unittest
 from axiom import Client
 
 
-def test_ingest():
-    """Tests the ingest endpoint"""
-    client = Client(os.getenv("AXIOM_DEPLOYMENT_URL"), os.getenv("AXIOM_TOKEN"), os.getenv('AXIOM_ORG_ID'))
-    print(
-        client.datasets.ingest(
-            os.getenv("AXIOM_DATASET"), [{"foo": "bar"}, {"bar": "baz"}]
+class TestDatasets(unittest.TestCase):
+    def setUp(self):
+        self.client = Client(os.getenv("AXIOM_DEPLOYMENT_URL"), os.getenv("AXIOM_TOKEN"), os.getenv('AXIOM_ORG_ID'))
+
+    def test_ingest(self):
+        """Tests the ingest endpoint"""
+        print(
+            self.client.datasets.ingest(
+                os.getenv("AXIOM_DATASET"), [{"foo": "bar"}, {"bar": "baz"}]
+            )
         )
-    )
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,16 +1,18 @@
 """This module contains test for the logging Handler."""
 import os
 import logging
+import unittest
 from axiom import Client
 from axiom.logging import AxiomHandler
 
 
-def test_log():
-    """Tests a simple log"""
-    client = Client(os.getenv("AXIOM_DEPLOYMENT_URL"), os.getenv("AXIOM_TOKEN"))
-    axiom_handler = AxiomHandler(client, os.getenv("AXIOM_DATASET"))
+class TestLogger(unittest.TestCase):
+    def test_log(self):
+        """Tests a simple log"""
+        client = Client(os.getenv("AXIOM_DEPLOYMENT_URL"), os.getenv("AXIOM_TOKEN"))
+        axiom_handler = AxiomHandler(client, os.getenv("AXIOM_DATASET"))
 
-    root = logging.getLogger()
-    root.addHandler(axiom_handler)
+        root = logging.getLogger()
+        root.addHandler(axiom_handler)
 
-    root.warning("foo")
+        root.warning("foo")
