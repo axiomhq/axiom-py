171eaf5407b6742a8a617eaa6363e96d20b8367d
Islam Shehata <49557793+schehata@users.noreply.github.com>
2021-11-18 18:13:56 +0100

    fix logger tests

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -2,17 +2,32 @@
 import os
 import logging
 import unittest
-from axiom import Client
+from .helpers import get_random_name
+from axiom import Client, DatasetCreateRequest
 from axiom.logging import AxiomHandler
 
 
 class TestLogger(unittest.TestCase):
     def test_log(self):
         """Tests a simple log"""
-        client = Client(os.getenv("AXIOM_DEPLOYMENT_URL"), os.getenv("AXIOM_TOKEN"), os.getenv("AXIOM_ORG_ID"))
-        axiom_handler = AxiomHandler(client, os.getenv("AXIOM_DATASET"))
+        client = Client(
+            os.getenv("AXIOM_DEPLOYMENT_URL"),
+            os.getenv("AXIOM_TOKEN"),
+            os.getenv("AXIOM_ORG_ID"),
+        )
+        # create a dataset for that purpose
+        dataset_name = get_random_name()
+        req = DatasetCreateRequest(
+            name=dataset_name,
+            description="a dataset to test axiom-py logger",
+        )
+        client.datasets.create(req)
+
+        axiom_handler = AxiomHandler(client, dataset_name)
 
         root = logging.getLogger()
         root.addHandler(axiom_handler)
 
         root.warning("foo")
+        # cleanup created dataset
+        client.datasets.delete(dataset_name)
