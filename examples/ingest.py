6e16ba8e9c6909a2879bfbba7ed45d9120f45095
Arne Bahlo <arne@axiom.co>
2024-09-08 13:46:23 +0200

    Move examples to single top-level file

--- examples/ingest.py
+++
@@ -1,8 +0,0 @@
-from axiom import Client
-
-
-def ingest(dataset_name):
-    client = Client()
-    res = client.ingest_events(dataset_name, [{"foo": "bar"}])
-    print(f"Ingested {len(res.ingested)} events with {
-          len(res.failures)} failures")
